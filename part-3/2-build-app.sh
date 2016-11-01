cb782d518a697e37322c3b3a2f30a8f043d9f6a9
Chris Felix <chrisfel@ca.ibm.com>
2016-11-01 14:16:02 -0400

    Adding commands to pull images on nodes
    Will result in faster startup when a container needs to be started up
    on a node that does not have its image already

--- part-3/2-build-app.sh
+++ part-3/2-build-app.sh
@@ -14,3 +14,26 @@ docker build --rm -t cascon/elk-logstash ./elk/logstash
 docker push cascon/elk-logstash
 docker build --rm -t cascon/elk-kibana ./elk/kibana
 docker push cascon/elk-kibana
+
+#Pull images on nodes to make container startup faster
+eval $(docker-machine env manager-1)
+docker pull cascon/db
+docker pull cascon/gateway
+docker pull cascon/strongloop
+docker pull cascon/elk-elasticsearch
+docker pull cascon/elk-logstash
+docker pull cascon/elk-kibana
+eval $(docker-machine env worker-1)
+docker pull cascon/db
+docker pull cascon/gateway
+docker pull cascon/strongloop
+docker pull cascon/elk-elasticsearch
+docker pull cascon/elk-logstash
+docker pull cascon/elk-kibana
+eval $(docker-machine env worker-2)
+docker pull cascon/db
+docker pull cascon/gateway
+docker pull cascon/strongloop
+docker pull cascon/elk-elasticsearch
+docker pull cascon/elk-logstash
+docker pull cascon/elk-kibana
